3b2aec3baf08f86639c7a422e9b6b4ee9b25000c
dante092 <gregborrelly@gmail.com>
2016-04-16 00:26:10 -0400

    Update and rename findthecode.py to citicodes.py

--- citicodes.py
+++ citicodes.py
@@ -1,22 +1,28 @@
-""" Small Script to find codes for MEGABUS"""
+""" Small Script to find citi destinations codes for MEGABUS"""
+
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-number = 89
+
+number = 89 # First city. 
 while True:
     try:
         url = 'http://us.megabus.com/JourneyResults.aspx?originCode={0}&destinationCode=143&outboundDepartureDate=4%2f16%2f2016&inboundDepartureDate=4%2f16%2f2016&passengerCount=2&transportType=0&concessionCount=0&nusCount=0&outboundWheelchairSeated=0&outboundOtherDisabilityCount=0&inboundWheelchairSeated=0&inboundOtherDisabilityCount=0&outboundPcaCount=0&inboundPcaCount=0&promotionCode=&withReturn=1'.format(number)
         html = urlopen(url)
         soup = BeautifulSoup(html)
         places = []
-        for place in soup.findAll("strong"):
+        for place in soup.findAll("strong"):# City name is in between a strong tag
             places.append(place.getText())
-        print("'"+places[0].upper()+"'"+' : '+"'"+str(number)+"'"+',')
+        print("'"+places[0].upper()+"'"+' : '+"'"+str(number)+"'"+',') # formats the city into a dictionary to be used later.
         number += 1
 
-    except IndexError:
+    except IndexError: #Some cities skip a digit or two, this code stops the indexerror from stopping the program.
         number +=1
         continue
     else:
-        if number > 145:
+        if number > 145: # This is the numerical code for the last city. 
             print('Done')
             break
+"""" 
+Sample Output : 
+
+""""
