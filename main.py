4cb692bc9069ad115efdd68c665d04e50cf6b827
dante092 <gregborrelly@gmail.com>
2016-04-20 01:10:36 -0400

    Added files via upload

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
-import megabus
+import shelve
 import time
+import megabus
+morning = shelve.open('morning_prices')
+afternoon = shelve.open('Afternoon_prices')
 
-print('MEGABUS CRAWLER')
-url = megabus.params('New York, ny', 'Boston, MA', '4/20/2016', '4/20/2016')
+print('MEGABUS CRAWLER'.center(70, '='))
+url = megabus.format('New York, ny', 'Boston, MA', '4/20/2016', '4/20/2016')
 
+#url = megabus.get_params()
 time.sleep(3)
 html = megabus.download_data(url)
+
+print('|SEARCHING FOR TRIP TO|')
 megabus.params_message(html)
-trip1 = megabus.download_trips(url)
 
-
-
-for i in trip1:
-    data = megabus.Trip(i)
-    print(i)
-    data.build_trip()
+id = 0
+while True:
+    trip = megabus.download_trips(url, id)
+    if trip == []:
+        print('No more trips for the day.')
+        break
+    megabus.format_trip('5')
+    for row in trip:
+        data = megabus.Trip(row, id)
+        data.build_trip()
+    id += 1
 
 
  # Need to get time and 
--- megabus.py
+++ megabus.py
@@ -1,8 +1,7 @@
 """ Create a program that texts or emails me everytime if finds a cheap ticket in megabus.com
 """
 import re
-import os
-import sys
+import shelve
 from urllib.request import urlopen
 from  urllib.error import URLError
 import urllib.request 
@@ -13,103 +12,118 @@ from bs4 import BeautifulSoup
 class Trip():
     """ Models a megabus trip.
     """
-    def __init__(self, data):
+    def __init__(self, data, number, prices=[]):
         self.data = data
+        self.trip_number = number
+        self.prices = prices
+
 
     def price(self):
         """
         Gets the price of the current trip
         :return: price = int
         """
+        prices = self.prices
         data = self.data
         price_regex = re.compile(r"\$\d\d")
         matches = price_regex.findall(data)
         price = matches[0]
-        print(price)
+        print('Price: ', price)
+        price = price.replace('$', '')
+        prices.append(int(price))
         return price
 
     def departure_time(self):
         data = self.data
-        departure_regex = re.compile(r"^(Departs\d+:\d\d)")
+        departure_regex = re.compile(r"^(Departs\d+:\d\d...)")
         matches = departure_regex.findall(data)
         departure_time = matches[0]
-        print(departure_time)
+        departure_time = departure_time.replace('Departs', '')
+        print('Departing: ', departure_time)
+        return departure_time
 
     def arrival_time(self):
         data = self.data
-        arrival_regex = re.compile(r"(Arrives\d+:\d\d)")
+        arrival_regex = re.compile(r"(Arrives\d+:\d\d...)")
         matches = arrival_regex.findall(data)
         arrival_time = matches[0]
-        print(arrival_time)
+        arrival_time = arrival_time.replace('Arrives','')
+        print('Arriving: ', arrival_time)
+        return arrival_time
 
     def build_trip(self):
+        print('\n')
+        print(' Trip {0} '.center(50, '=').format(self.trip_number))
         self.price()
-        self.arrival_time()
         self.departure_time()
-
-
-
+        self.arrival_time()
 
 def generate_city_code(citi):
+    """
+    :return: The proper city code, string.
+    """
     citi = citi.strip()
     citi = citi.upper()
     citi_codes = {
-        'ALBANY, NY' : '89',
-        'AMHERST, MA' : '90',
-        'ANN ARBOR, MI' : '91',
-        'ATLANTIC CITY, NJ' : '92',
-        'BINGHAMTON, NY' : '93',
-        'BOSTON, MA' : '94',
-        'BUFFALO, NY' : '95',
-        'BURLINGTON, VT' : '96',
-        'CAMDEN' : '97',
-        'CHAMPAIGN, IL' : '98',
-        'CHARLOTTE, NC' : '99',
-        'CHICAGO, IL' : '100',
-        'CHRISTIANSBURG, VA' : '101',
-        'CINCINNATI, OH' : '102',
-        'CLEVELAND, OH' : '103',
-        'COLUMBIA, MO' : '104',
-        'COLUMBUS, OH' : '105',
-        'DES MOINES, IA' : '106',
-        'DETROIT, MI' : '107',
-        'ERIE, PA' : '108',
-        'FREDERICK, MD' : '109',
-        'HAMPTON, VA' : '110',
-        'HARRISBURG, PA' : '111',
-        'HARTFORD, CT' : '112',
-        'HOLYOKE, CT' : '113',
-        'HYANNIS, MA' : '114',
-        'INDIANAPOLIS, IN' : '115',
-        'IOWA CITY, IA' : '116',
-        'KANSAS CITY, MO' : '117',
-        'KNOXVILLE, TN' : '118',
-        'MADISON, WI' : '119',
-        'MEMPHIS, TN' : '120',
-        'MILWAUKEE, WI' : '121',
-        'NEW HAVEN, CT' : '122',
-        'NEW YORK, NY' : '123',
-        'NIAGARA FALLS, ON' : '124',
-        'NORMAL, IL' : '125',
-        'OMAHA, NE' : '126',
-        'PHILADELPHIA, PA' : '127',
-        'PITTSBURGH, PA' : '128',
-        'PORTLAND, ME' : '129',
-        'PROVIDENCE, RI' : '130',
-        'DURHAM, NC' : '131',
-        'RICHMOND, VA' : '132',
-        'RIDGEWOOD, NJ' : '133',
-        'ROCHESTER, NY' : '134',
-        'SECAUCUS, NJ' : '135',
-        'ST LOUIS, MO' : '136',
-        'STATE COLLEGE, PA' : '137',
-        'STORRS, CT' : '138',
-        'SYRACUSE, NY' : '139',
-        'TOLEDO, OH' : '140',
-    }
+            'ALBANY, NY' : '89',
+            'AMHERST, MA' : '90',
+            'ANN ARBOR, MI' : '91',
+            'ATLANTIC CITY, NJ' : '92',
+            'BINGHAMTON, NY' : '93',
+            'BOSTON, MA' : '94',
+            'BUFFALO, NY' : '95',
+            'BURLINGTON, VT' : '96',
+            'CAMDEN' : '97',
+            'CHAMPAIGN, IL' : '98',
+            'CHARLOTTE, NC' : '99',
+            'CHICAGO, IL' : '100',
+            'CHRISTIANSBURG, VA' : '101',
+            'CINCINNATI, OH' : '102',
+            'CLEVELAND, OH' : '103',
+            'COLUMBIA, MO' : '104',
+            'COLUMBUS, OH' : '105',
+            'DES MOINES, IA' : '106',
+            'DETROIT, MI' : '107',
+            'ERIE, PA' : '108',
+            'FREDERICK, MD' : '109',
+            'HAMPTON, VA' : '110',
+            'HARRISBURG, PA' : '111',
+            'HARTFORD, CT' : '112',
+            'HOLYOKE, CT' : '113',
+            'HYANNIS, MA' : '114',
+            'INDIANAPOLIS, IN' : '115',
+            'IOWA CITY, IA' : '116',
+            'KANSAS CITY, MO' : '117',
+            'KNOXVILLE, TN' : '118',
+            'MADISON, WI' : '119',
+            'MEMPHIS, TN' : '120',
+            'MILWAUKEE, WI' : '121',
+            'NEW HAVEN, CT' : '122',
+            'NEW YORK, NY' : '123',
+            'NIAGARA FALLS, ON' : '124',
+            'NORMAL, IL' : '125',
+            'OMAHA, NE' : '126',
+            'PHILADELPHIA, PA' : '127',
+            'PITTSBURGH, PA' : '128',
+            'PORTLAND, ME' : '129',
+            'PROVIDENCE, RI' : '130',
+            'DURHAM, NC' : '131',
+            'RICHMOND, VA' : '132',
+            'RIDGEWOOD, NJ' : '133',
+            'ROCHESTER, NY' : '134',
+            'SECAUCUS, NJ' : '135',
+            'ST LOUIS, MO' : '136',
+            'STATE COLLEGE, PA' : '137',
+            'STORRS, CT' : '138',
+            'SYRACUSE, NY' : '139',
+            'TOLEDO, OH' : '140',
+        }
     return citi_codes[citi]
 
 def generate_date(date):
+    """
+    Formats the provided date, returns: String
+    """
     date = date
     date = date.replace('/', '\n')
     date = date.split()
@@ -122,12 +136,10 @@ def get_params():
     destination = input('Destination: ')
     leaving = input('Date of Departure: ')
     arrival = input('Date of Arrival: ')
-    url = megabus.params(origin, destination, leaving, arrival)
+    url = format(origin, destination, leaving, arrival)
     return url
 
-
-
-def params(origin, destination, leaving, comingback, passengers = '2' ):
+def format(origin, destination, leaving, comingback, passengers = '2' ):
     """ Formats a Megabus URL with the destination information."""
     base = 'http://us.megabus.com/JourneyResults.aspx?'
     origincode = 'originCode=' + generate_city_code(origin)
@@ -143,10 +155,13 @@ def download_data(url):
     headers = {}
     headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
     request  = urllib.request.Request(url, headers=headers)
-    try:
-        connect = urlopen(request)
-    except URLError as e:
-        print(e)
+    while True:
+        try:
+            connect = urlopen(request)
+            break
+        except URLError as e:
+            print('Something is wrong', e)
+            continue
 
     soup = BeautifulSoup(connect, 'html.parser')
     return soup
@@ -162,13 +177,20 @@ def params_message(soup):
         word = word.replace('\t','')
         word = word.replace('\n', '')
         print(word)
-        
-def download_trips(url):
+
+def format_trip(number):
+    number = str(number)
+    id ='JourneyResylts_OutboundList_GridViewResults_ctl09_row_item'
+    id = id.replace('9', number)
+    return id
+
+def download_trips(url, id):
+    identification = format_trip(id)
     html = download_data(url)
     temp = []
     trip = [] 
-    for i in html.findAll('ul', id = 'JourneyResylts_OutboundList_GridViewResults_ctl03_row_item'):
-        temp.append(i.getText())
+    for trip_data in html.findAll('ul', id = identification):
+        temp.append(trip_data.getText())
     for word in temp:
         word = word.replace('\t','')
         word = word.replace('\n', '')
